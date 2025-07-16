e2157e485851ff0e854be583ca4c251f0242079f
Joko Prasetyo <96661693+mrjokoprastiyo@users.noreply.github.com>
2025-07-16 15:26:19 +0700

    Add files via upload

---
+++ js/install.js
@@ -0,0 +1,16 @@
+let deferredPrompt;
+const installBtn = document.getElementById('installBtn');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  installBtn.style.display = 'block';
+
+  installBtn.addEventListener('click', () => {
+    installBtn.style.display = 'none';
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      deferredPrompt = null;
+    });
+  });
+});
